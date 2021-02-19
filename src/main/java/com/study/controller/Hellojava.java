65cd89b2938c0e3d540c5ea4c746a936e08266a8
Sandesh Ramchandra Gadhave <sandesh.gadhave@yash.com>
2021-02-19 13:22:17 +0530

    Project added

---
+++ src/main/java/com/study/controller/Hellojava.java
@@ -0,0 +1,11 @@
+package com.study.controller;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Everyone");
+
+	}
+
+}
